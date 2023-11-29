b36edf6fe4efbafd977e61d0833f70f5929580c6
Nicolas07068 <nicolacocao@gmail.com>
2023-11-28 21:56:26 -0300

    filtro de tarefas completas, completo

--- index.js
+++ index.js
@@ -97,6 +97,31 @@ app.get('/ativas', (requisicao , resposta) =>{
     })
 })
 
+app.get('/completas', (requisicao, resposta) => {
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = '1'
+    `
+
+    conexao.query(sql, (erro, dados) =>{
+        if (erro){
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) =>{
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: true
+            }
+        })
+
+        const quantidadeTarefas= tarefas.length
+
+        resposta.render('completas', {tarefas, quantidadeTarefas})
+    })
+})
+
 app.get('/', (requisicao, resposta) => {
     const sql ='SELECT * FROM tarefas'
 
